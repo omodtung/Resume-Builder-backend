612d9b8765a2a3b93fc34bec255cf620080f714e
TungDT <dothedung131@gmail.com>
2025-04-22 00:34:59 +0700

    out user id

--- src/main/java/saigonuni/dev/resumeBuilder/dto/Auth/AutheticationResponse.java
+++ src/main/java/saigonuni/dev/resumeBuilder/dto/Auth/AutheticationResponse.java
@@ -10,12 +10,13 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-
-
 public class AutheticationResponse {
-    
-     @JsonProperty("access_token")
+
+  @JsonProperty("access_token")
   private String accessToken;
-  @JsonProperty("refresh_token")  
+
+  @JsonProperty("refresh_token")
   private String refreshToken;
+
+  private Long userId; // Add userId field
 }
