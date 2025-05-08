6805258657081e34e68433b0f6c859bd42754d8e
TungDT <dothedung131@gmail.com>
2025-05-08 14:36:00 +0700

    open auth

--- src/main/java/saigonuni/dev/resumeBuilder/config/SecurityConfig.java
+++ src/main/java/saigonuni/dev/resumeBuilder/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig implements WebMvcConfigurer {
       .disable()
       .authorizeHttpRequests()
       .requestMatchers(
+        "/",
         "/register",
         "/auth/authenticate/**",
         "/auth/refresh-token",
